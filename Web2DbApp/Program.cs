1c6f415da42d1efd7a445398f2784af5d0b8a5da
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-11-14 10:48:29 +0100

    Created a method that prints all persons

--- Web2DbApp/Program.cs
+++ Web2DbApp/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Web2DbApp.Entities;
 
 namespace Web2DbApp
 {
@@ -10,6 +10,21 @@ namespace Web2DbApp
     {
         static void Main(string[] args)
         {
+            List<Person> persons = new List<Person>();
+            PrintAll(persons);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintAll(List<Person> persons)
+        {
+            foreach(Person person in persons)
+            {
+                Console.WriteLine("***************************************************");
+                Console.WriteLine($"First name: {person.FirstName}");
+                Console.WriteLine($"Last name: {person.LastName}");
+                Console.WriteLine($"Title of courtesy: {person.TitleOfCourtesy}");
+            }
         }
     }
 }
