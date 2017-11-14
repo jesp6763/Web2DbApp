e5592330c5efd602fccc4b4131494a90da3a16b1
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-11-14 10:48:49 +0100

    Created DataAccess project

---
+++ Web2DbApp.DataAccess/Executor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Web2DbApp.DataAccess
+{
+    public class Executor
+    {
+        private const string connectionString = "";
+
+        public Executor()
+        {
+
+        }
+
+        public void Execute(string procedureName, string sqlQuery)
+        {
+
+        }
+
+        public DataSet Execute(string stringQuery)
+        {
+            return new DataSet();
+        }
+    }
+}
---
+++ Web2DbApp.DataAccess/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Web2DbApp.Entities;
+
+namespace Web2DbApp.DataAccess
+{
+    public class Repository
+    {
+        private Executor executor;
+
+        public Repository()
+        {
+            executor = new Executor();
+        }
+
+        /// <summary>
+        /// Gets all person from database
+        /// </summary>
+        /// <returns>A list of persons found</returns>
+        public List<Person> GetAll()
+        {
+            return new List<Person>();
+        }
+
+        /// <summary>
+        /// Saves a list of persons to the database
+        /// </summary>
+        /// <param name="persons"></param>
+        public void Save(List<Person> persons)
+        {
+
+        }
+    }
+}
