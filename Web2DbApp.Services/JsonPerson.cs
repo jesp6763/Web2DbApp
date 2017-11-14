7273bf23796b78dc47fb2244729538a3937c87d6
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-11-14 10:49:44 +0100

    Created services project

---
+++ Web2DbApp.Services/JsonPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Web2DbApp.Entities;
+
+namespace Web2DbApp.Services
+{
+    class JsonPerson
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Title { get; set; }
+
+        public static explicit operator Person(JsonPerson jPerson)
+        {
+            return new Person(jPerson.First, jPerson.Last, jPerson.Title);
+        }
+    }
+}
---
+++ Web2DbApp.Services/MockDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Web2DbApp.Entities;
+
+namespace Web2DbApp.Services
+{
+    public class MockDataProvider
+    {
+        private string url;
+
+        public List<Person> GetPeople(int amount)
+        {
+            return new List<Person>();
+        }
+    }
+}
