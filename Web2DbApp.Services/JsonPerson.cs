54f25aae783453dd2f8ff35a9f31c7b8614c7cd7
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-11-15 09:54:08 +0100

    Added a simple validation to check if the first letter in the firstname is capital

--- Web2DbApp.Services/JsonPerson.cs
+++ Web2DbApp.Services/JsonPerson.cs
@@ -1,4 +1,5 @@
 ﻿using Web2DbApp.Entities;
+using AspITCV.Validators.CommonEntities;
 
 namespace Web2DbApp.Services
 {
@@ -10,7 +11,30 @@ namespace Web2DbApp.Services
 
         public static explicit operator Person(JsonPerson jPerson)
         {
-            return new Person(jPerson.First, jPerson.Last, jPerson.Title);
+            Person person = new Person(jPerson.First, jPerson.Last, jPerson.Title);
+            #region Validation
+            (bool isValid, string errorMessage) validationResult = PersonValidations.IsNameValid($"{jPerson.First} {jPerson.Last}");
+
+            if(!validationResult.isValid)
+            {
+                if(char.IsWhiteSpace(person.FirstName[0]))
+                {
+                    person.FirstName.TrimStart(' ');
+                }
+                
+                char[] firstNameCharacters = person.FirstName.ToCharArray();
+
+                // Make first letter uppercase if it is a lowercase
+                if(char.IsLower(firstNameCharacters[0]))
+                {
+                    firstNameCharacters[0] = char.ToUpper(firstNameCharacters[0]);
+                }
+
+                person.FirstName = new string(firstNameCharacters);
+            }
+            #endregion
+
+            return person;
         }
     }
 }
